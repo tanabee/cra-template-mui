3f215679ae24a816232ee17c2beddb89c0848f54
tanabee <tanabee1986@gmail.com>
2022-02-13 16:28:19 +0900

    Upgrade mui version to 5

--- template/src/App.js
+++ template/src/App.js
@@ -1,40 +1,52 @@
-import React from 'react'
-import { useTheme } from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
+import AddIcon from '@mui/icons-material/Add'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Fab from '@mui/material/Fab'
+import IconButton from '@mui/material/IconButton'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
 
 export default function App() {
-  const { spacing } = useTheme()
-
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">cra-template-mui</Typography>
-        </Toolbar>
-      </AppBar>
-      <div style={{ padding: spacing(2) }}>
-        <Typography style={{ marginBottom: spacing(1) }} variant="h1">
-          h1. Heading
-        </Typography>
-        <Typography style={{ marginBottom: spacing(1) }} variant="h2">
-          h2. Heading
-        </Typography>
-        <Typography style={{ marginBottom: spacing(1) }} variant="h3">
-          h3. Heading
-        </Typography>
-        <Typography style={{ marginBottom: spacing(1) }} variant="body1">
-          body1. text
-        </Typography>
-        <Typography style={{ marginBottom: spacing(1) }} variant="body2">
-          body2. text
-        </Typography>
-        <Typography style={{ marginBottom: spacing(1) }} variant="caption" component="p">
-          caption. text
-        </Typography>
-        <a href="https://github.com/tanabee/cra-template-mui">tanabee/cra-template-mui</a>
-      </div>
-    </div>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              cra-template-mui
+            </Typography>
+            <IconButton
+              size="large"
+              edge="end"
+              color="inherit"
+              onClick={() => window.open('https://github.com/tanabee/cra-template-mui', '_blank')}
+            >
+              <GitHubIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box sx={{ p: 3 }}>
+        <Typography variant="h1">h1. Heading</Typography>
+        <Typography variant="h2">h2. Heading</Typography>
+        <Typography variant="h3">h3. Heading</Typography>
+        <Typography variant="h4">h4. Heading</Typography>
+        <Typography variant="h5">h5. Heading</Typography>
+        <Typography variant="h6">h6. Heading</Typography>
+        <Typography variant="body1">body1. text</Typography>
+        <Typography variant="body2">body2. text</Typography>
+      </Box>
+      <Fab
+        color="secondary"
+        sx={{
+          position: 'fixed',
+          right: ({ spacing }) => spacing(3),
+          bottom: ({ spacing }) => spacing(3),
+        }}
+      >
+        <AddIcon />
+      </Fab>
+    </>
   )
 }
